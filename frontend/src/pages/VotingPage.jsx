f1af2c24b49f6725f9d2bda6aa53ab125c59ecc1
ricardoMogas <al066208@uacam.mx>
2024-06-07 20:23:00 -0600

    Update votingPage

--- frontend/src/pages/VotingPage.jsx
+++ frontend/src/pages/VotingPage.jsx
@@ -4,7 +4,7 @@ const PrincipalPage = () => {
     
     return (
         <div>
-            <h1 style={{ textAlign: 'center', color: 'blue' }}>Página Principal</h1>
+            <h1 style={{ textAlign: 'center', color: 'blue' }}>Página Para votar</h1>
             <p style={{ fontSize: '18px', textAlign: 'center' }}>¡Bienvenido a nuestra página principal!</p>
             {/* Aquí puedes agregar el contenido de tu página */}
         </div>
